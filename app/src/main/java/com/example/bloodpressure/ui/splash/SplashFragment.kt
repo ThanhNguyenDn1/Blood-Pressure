4129b8278a5d353eb8ae1f751e93e29867335810
THANH\thanh <thanhnguyen100101@gmail.com>
2022-12-07 07:11:12 +0700

    home

--- app/src/main/java/com/example/bloodpressure/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/bloodpressure/ui/splash/SplashFragment.kt
@@ -5,17 +5,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.bloodpressure.base.BaseFragment
 import com.example.bloodpressure.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashFragment : BaseFragment<SplashViewModel, FragmentSplashBinding>() {
     override val viewModel: SplashViewModel by viewModels()
 
-
-    override fun provideViewBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentSplashBinding {
-        TODO("Not yet implemented")
-    }
+    override fun provideViewBinding(inflater: LayoutInflater, container: ViewGroup?) =
+        FragmentSplashBinding.inflate(LayoutInflater.from(inflater.context), container, false)
 
 
 }
